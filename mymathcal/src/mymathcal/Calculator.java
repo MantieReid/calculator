0dc9869c9307d62d479e75bd0cba97ba25c7fabd
KingIonTrueLove <KingIonTrueLove@gmail.com>
2018-03-28 22:14:43 -0700

    Update Calculator.java
    Cleaned up whitespace, Added error check for hitting equals button multiple times in a row.

--- mymathcal/src/mymathcal/Calculator.java
+++ mymathcal/src/mymathcal/Calculator.java
@@ -26,7 +26,8 @@ public class Calculator extends javax.swing.JFrame {
     
     
     
-    double num,ans;
+    double num,something;
+    boolean equaled = false;
     int calulcation;
     
     public Calculator() {
@@ -36,31 +37,35 @@ public class Calculator extends javax.swing.JFrame {
     
     public  void  arithmetic_operation()
     {
+        if (equaled == false) {
+            something = Double.parseDouble(jTextField1.getText());
+           }
+        
         switch(calulcation)
-        {
-            case 1: //addition 
-                ans = num + Double.parseDouble(jTextField1.getText()); //converts string to double
+        {      
+                case 1: // addition 
+                num = num + something; //converts string to double
                 //alllows for it to be added to. 
-                jTextField1.setText(Double.toString(ans)); //converets double to string. 
+                jTextField1.setText(Double.toString(num)); //converets double to string.
                 break;
                 
-                case 2: //subtraction
-                ans = num - Double.parseDouble(jTextField1.getText()); //converts string to double. 
-                jTextField1.setText(Double.toString(ans)); //converets double to string. 
+                case 2: // subtraction
+                num = num - something; //converts string to double. 
+                jTextField1.setText(Double.toString(num)); //converets double to string. 
                 break;
                 
                  case 3: // Mutiplication
-                ans = num * Double.parseDouble(jTextField1.getText()); //converts string to double. 
-                jTextField1.setText(Double.toString(ans)); //converets double to string. 
+                num = num * something; //converts string to double. 
+                jTextField1.setText(Double.toString(num)); //converets double to string. 
                 break;
                 
                 
                   case 4: // division
-                ans = num / Double.parseDouble(jTextField1.getText()); //converts string to double. 
-                jTextField1.setText(Double.toString(ans)); //converets double to string. 
-                break;
-                
+                num = num / something; //converts string to double. 
+                jTextField1.setText(Double.toString(num)); //converets double to string. 
+                break; 
         }
+        equaled = true;
     }
     
     
@@ -74,12 +79,7 @@ public class Calculator extends javax.swing.JFrame {
            buttonlist[i].setBackground(Color.red);
        }
    }
-    
-   
-    
-    
-    
-    
+
     //Method that enables all buttons, Except for the on and off buttons. 
     public void enable()
     {
@@ -96,42 +96,8 @@ public class Calculator extends javax.swing.JFrame {
       }
       
        On.setEnabled(false); //on button disable 
-      
-// Commented out, No longer needed. 
-
-// Better to use Array to diasble all buttons. 
-
-//     jTextField1.setEnabled(true); //diasbles the textfield
-//        Number1.setEnabled(true);
-//        Number2.setEnabled(true);
-//        Number3.setEnabled(true);
-//        Number4.setEnabled(true);
-//        Number5.setEnabled(true);
-//        Number6.setEnabled(true);
-//        Number7.setEnabled(true);
-//        Number8.setEnabled(true);
-//        Number9.setEnabled(true);
-//        NumberZero.setEnabled(true);
-//        Add.setEnabled(true);
-//        minus.setEnabled(true);
-//        //minus.setEnabled(false);
-//        Divide.setEnabled(true);
-//        times.setEnabled(true);
-//        Equals.setEnabled(true);
-//        Clear.setEnabled(true);
-//        backspace.setEnabled(true);
-//        decimial.setEnabled(true);
-//       
-
-        
     }
     
-
-
-  
-    
-   
-
     //method that disables the buttons. Except for the on button
     public void disable()
     {
@@ -142,37 +108,8 @@ public class Calculator extends javax.swing.JFrame {
       {
           buttonlist[i].setEnabled(false);
       }
-      
-       
-
- //No longer needed, Its better to use an array. Much more shorter and easier.        
-//        jTextField1.setEnabled(false); //diasbles the textfield
-//        Number1.setEnabled(false);
-//        Number2.setEnabled(false);
-//        Number3.setEnabled(false);
-//        Number4.setEnabled(false);
-//        Number5.setEnabled(false);
-//        Number6.setEnabled(false);
-//        Number7.setEnabled(false);
-//        Number8.setEnabled(false);
-//        Number9.setEnabled(false);
-//        NumberZero.setEnabled(false);
-//        Add.setEnabled(false);
-//        minus.setEnabled(false);
-//        //minus.setEnabled(false);
-//        Divide.setEnabled(false);
-//        times.setEnabled(false);
-//        Equals.setEnabled(false);
-//        Clear.setEnabled(false);
-//        backspace.setEnabled(false);
-//        decimial.setEnabled(false);
-//        
-        
-        
-        
     }
     
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -486,14 +423,14 @@ public class Calculator extends javax.swing.JFrame {
                     .addComponent(Number3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Divide, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(NumberZero, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(decimial, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(Equals, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel2))))
+                        .addComponent(jLabel2))
+                    .addComponent(NumberZero, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
         pack();
@@ -503,27 +440,40 @@ public class Calculator extends javax.swing.JFrame {
         // TODO add your handling code here:
         jTextField1.setText(""); //sets textbox to Nothing.
         //removes all text from the text box. 
-        
     }//GEN-LAST:event_ClearActionPerformed
 
     private void offActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_offActionPerformed
         // TODO add your handling code here:
         disable();// disables all buttons, except for on button.
-        
-        
     }//GEN-LAST:event_offActionPerformed
 
     private void AddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddActionPerformed
-        // TODO add your handling code here:
-         //jTextField1.setText(Double.toString(ans)); //converets double to string. 
          num = Double.parseDouble(jTextField1.getText());
          calulcation = 1; //refers to case 1 addtion 
          jTextField1.setText(""); // clears text field
-        // jLabel1.setText(num + "+"); //sets text with num and +
-         
-         
-
+         equaled = false;
     }//GEN-LAST:event_AddActionPerformed
+
+    private void minusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minusActionPerformed
+         num = Double.parseDouble(jTextField1.getText());
+         calulcation = 2; //refers to case 2 subtration  
+         jTextField1.setText(""); // clears text field
+         equaled = false;
+    }//GEN-LAST:event_minusActionPerformed
+
+    private void timesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_timesActionPerformed
+         num = Double.parseDouble(jTextField1.getText());
+         calulcation = 3; //refers to case mutpily
+         jTextField1.setText(""); // clears text field
+         equaled = false;
+    }//GEN-LAST:event_timesActionPerformed
+    
+    private void DivideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DivideActionPerformed
+         num = Double.parseDouble(jTextField1.getText());
+         calulcation = 4; //refers to case 4 division
+         jTextField1.setText(""); // clears text field
+         equaled = false;
+    }//GEN-LAST:event_DivideActionPerformed
 
     private void Number7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number7ActionPerformed
         // TODO add your handling code here:
@@ -543,13 +493,6 @@ public class Calculator extends javax.swing.JFrame {
 
     }//GEN-LAST:event_Number8ActionPerformed
 
-    private void minusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minusActionPerformed
-     num = Double.parseDouble(jTextField1.getText());
-         calulcation = 2; //refers to case 2 subtration  
-         jTextField1.setText(""); // clears text field
-        // jLabel1.setText(num + "-"); //sets text with num and -
-    }//GEN-LAST:event_minusActionPerformed
-
     private void OnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OnActionPerformed
         // TODO add your handling code here:
         enable(); //enables all buutons. 
@@ -557,32 +500,17 @@ public class Calculator extends javax.swing.JFrame {
 
     private void Number4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number4ActionPerformed
         // TODO add your handling code here:
-      jTextField1.setText(jTextField1.getText() + "4"); 
-//sets text field to four and places it next to number placed on screen.
-//testchangecolors(); //It works!
+      jTextField1.setText(jTextField1.getText() + "4");
 
     }//GEN-LAST:event_Number4ActionPerformed
 
     private void Number5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number5ActionPerformed
-        // TODO add your handling code here:
-     jTextField1.setText(jTextField1.getText() + "5"); //sets text field to five and places it next to number placed on screen.
-
+     jTextField1.setText(jTextField1.getText() + "5");
     }//GEN-LAST:event_Number5ActionPerformed
 
-    private void Number6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number6ActionPerformed
-        // TODO add your handling code here:
-     jTextField1.setText(jTextField1.getText() + "6"); //sets text field to six and places it next to number placed on screen.
-
-    }//GEN-LAST:event_Number6ActionPerformed
-
-    private void timesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_timesActionPerformed
-        // TODO add your handling code here:
- num = Double.parseDouble(jTextField1.getText());
-         calulcation = 3; //refers to case mutpily
-         jTextField1.setText(""); // clears text field
-         //jLabel1.setText(num + "*"); //sets text with num  and *
-        
-    }//GEN-LAST:event_timesActionPerformed
+    private void Number6ActionPerformed(java.awt.event.ActionEvent evt) {                                        
+     jTextField1.setText(jTextField1.getText() + "6");
+    }
 
     private void Number1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number1ActionPerformed
         // TODO add your handling code here:
@@ -597,29 +525,17 @@ public class Calculator extends javax.swing.JFrame {
 
     private void Number3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Number3ActionPerformed
         // TODO add your handling code here:
- jTextField1.setText(jTextField1.getText() + "3"); //sets text field 3 four and places it next to number placed on screen.
-
+        jTextField1.setText(jTextField1.getText() + "3"); //sets text field 3 four and places it next to number placed on screen.
     }//GEN-LAST:event_Number3ActionPerformed
-
-    private void DivideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DivideActionPerformed
-        // TODO add your handling code here:
-         num = Double.parseDouble(jTextField1.getText());
-         calulcation = 4; //refers to case 4 division
-         jTextField1.setText(""); // clears text field
-         //jLabel1.setText(num + "/"); //sets text with num and /
-      
-    }//GEN-LAST:event_DivideActionPerformed
 
     private void NumberZeroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NumberZeroActionPerformed
         // TODO add your handling code here:
      jTextField1.setText(jTextField1.getText() + "0"); //sets text field to 0 and places it next to number placed on screen.
-
     }//GEN-LAST:event_NumberZeroActionPerformed
 
     private void decimialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decimialActionPerformed
         // TODO add your handling code here:
-      jTextField1.setText(jTextField1.getText() + "."); //sets text field to . and places it next to number placed on screen.
-
+        jTextField1.setText(jTextField1.getText() + "."); //sets text field to . and places it next to number placed on screen.
     }//GEN-LAST:event_decimialActionPerformed
 
     private void EqualsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EqualsActionPerformed
@@ -641,10 +557,7 @@ public class Calculator extends javax.swing.JFrame {
             back.deleteCharAt(number); //used to dekete a single a character from a spefcic postion. 
             store=back.toString();
             jTextField1.setText(store);
-            
-            
-            
-            
+               
         }
     }//GEN-LAST:event_backspaceActionPerformed
 
